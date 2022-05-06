b38da4f7ee7b5db06648ffcc02acdaf79263314c
Char Isabelo <charisseisabelo@gmail.com>
2022-05-05 23:09:35 -1000

    more changes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reactaws918f07f1": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { withAuthenticator, Authenticator } from "@aws-amplify/ui-react";
+import awsExports from "./aws-exports";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <Authenticator />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
